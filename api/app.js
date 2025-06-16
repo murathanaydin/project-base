50e10782d8f61b5387a08be1b3684fc3dcf035e8
Murathan Aydın <murathanaydin6134@gmail.com>
2025-06-16 20:39:56 +0300

    routing altyapisi kuruldu #2

--- api/app.js
+++ api/app.js
@@ -1,11 +1,11 @@
+if(process.env.NODE_ENV != "production")
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -19,9 +19,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next) => {
+  console.log("Ben app.js de tanimlanan bir middleware'im. ");
+  next();
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api',require('./routes/index'));
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
