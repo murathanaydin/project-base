70f7ef916e82e50a3efecec603d342aed28d74d2
Murathan Aydın <murathanaydin6134@gmail.com>
2025-06-29 19:47:31 +0300

    #7 user auth yapisi kuruldu

--- api/db/modules/Users.js
+++ api/db/modules/Users.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const Enume = require("../../config/Enume");
+const is = require("is_js");
+const CustomError = require("../../lib/Error");
+const bcrypt = require("bcrypt-nodejs");
 
 const schema = mongoose.Schema({
     email: {type:String, required: true},
@@ -16,6 +20,18 @@ const schema = mongoose.Schema({
 })
 
 class Users extends mongoose.Model {
+
+    validPassword(password) {
+        return bcrypt.compareSync(password, this.password);
+    }
+
+    static validateFieldsBeforeAuth(email, password) {
+        if(typeof password !== "string" || password.length < Enume.PASS_LENGTH || is.not.email(email)) {
+            throw new CustomError(Enume.HTTP_CODES.UNAUTHORIZED, "Validation Error" , "Email or Password wrong");
+        } else {
+            return null;
+        }
+    }
 
 }
 
